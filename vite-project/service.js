35569ff87f5d3ad3d2ae3aadc1402443b4849b8b
Julio Varas Contreras <jvarascontreras@gmail.com>
2024-11-07 21:50:00 -0300

    Ejemplo con buscador terminado.

--- vite-project/service.js
+++ vite-project/service.js
@@ -1,9 +1,11 @@
 const API_KEY = 'API_DEVELOPER_GIPHY'
-const searchQuery = 'cats'
+// la API_KEY de la API de Giphy
 
 // Función para hacer fetch de los datos
-async function fetchGifts() {
+async function fetchGifts(searchQuery) {
+    // endpoint search
     const searchEndpoint = `http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchQuery}`
+    // intenta obtener la respuesta
     try {
         // se hace un fetch al searchEnpoint
         const response = await fetch(searchEndpoint)
@@ -11,9 +13,9 @@ async function fetchGifts() {
         if (!response.ok) {
             throw new Error('error on service')
         }
-        // espera la respuesta y almacena en data
+        // espera la respuesta y la almanena en data
         const data = await response.json()
-       // console.log(data.data) 
+        // console.log(data.data)
         displayGifs(data.data)
     } catch (error) {
         console.error('there was an error:', error)
@@ -28,14 +30,24 @@ function displayGifs(gifs) {
     gifsContainer.innerHTML = ''
 
     // Función que recorre todo el objeto de gifs
-    gifs.forEach(gif => {
+    gifs.forEach((gif) => {
         // crea un elemento IMG en el DOM
-        const img = document.createElement('img');
+        const img = document.createElement('img')
         // la url de la imagen del gif
-        img.src = gif.images.fixed_height.url;
+        img.src = gif.images.fixed_height.url
         // añade al final de cada elemento la imagen
         gifsContainer.appendChild(img)
     })
 }
 
-fetchGifts()
+document.getElementById('search-form').addEventListener('submit', (event) => {
+    // Prevent Default evita que la página se recargue al enviar el formulario
+    event.preventDefault()
+    // Se obtiene el valor del input search y se eliminan los espacios en blanco con trim
+    const searchQuery = document.getElementById('search-input').value.trim()
+
+    if (searchQuery) {
+        // llamamos al fetchGifs con el término de busqueda ingresado
+        fetchGifts(searchQuery)
+    }
+})
